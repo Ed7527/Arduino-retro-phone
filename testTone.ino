1e2b6ddfe79277706b914df279e3d4d8554d0c54
Edward Torpy <edward.torpy@my.jcu.edu.au>
2017-07-04 21:18:48 +1000

    Add files via upload

---
+++ testTone.ino
@@ -0,0 +1,18 @@
+/*
+1kHZ test tone
+*/
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(10, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(10, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(10, LOW);
+  delayMicroseconds(500);
+}
