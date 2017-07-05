405e073469937df29810b92f6a31f7b0a82bcffa
Edward Torpy <edward.torpy@my.jcu.edu.au>
2017-07-05 20:24:35 +1000

    Add files via upload

---
+++ lcd.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  lcd.print("EdwardTorpy.com");
+  lcd.setCursor(0, 1);
+  lcd.print("Phone project");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
