d92549e456a8248522b65572616010dba45891b0
Edward Torpy <edward.torpy@my.jcu.edu.au>
2017-07-09 10:02:16 +1000

    Update _24HzRingTone.ino
    Sounds closer to the Australian ring

--- _24HzRingTone.ino
+++ _24HzRingTone.ino
@@ -1,10 +1,10 @@
 /*
-Generates a -9V to 9V square wave using a 9V battery and an H-Bridge.
+Generates a square wave using a DC power supply and an H-Bridge.  
+The amount of rings and delays are set to make it sound close to the Australian phone ring.
 */
 
 
 void setup() {
-  // initialize digital pin 10 as an output.
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(9, OUTPUT);
@@ -14,12 +14,19 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-  //delayMicroseconds(halfWavePeriod);
-  delay(20);
-  digitalWrite(2, HIGH);
-  digitalWrite(3, LOW);
-  //delayMicroseconds(halfWavePeriod);
-  delay(20);
+
+  for (int rings = 0; rings < 2; rings++) {
+    for (int bellHits = 0; bellHits < 20; bellHits++){
+      digitalWrite(2, LOW);
+      digitalWrite(3, HIGH);
+      delay(20);
+      digitalWrite(2, HIGH);
+      digitalWrite(3, LOW);
+      //delayMicroseconds(halfWavePeriod);
+      delay(20);
+    }
+    delay(150);
+  }
+
+  delay(800);
 }
