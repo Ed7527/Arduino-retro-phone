ce731563a73baa88348309e8110ce18c1a06f3be
Edward Torpy <edward.torpy@my.jcu.edu.au>
2017-07-11 21:08:55 +1000

    Add files via upload

---
+++ diallerDecoder.ino
@@ -0,0 +1,19 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.print("Number:\n");
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  digitalWrite(3, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(2) == LOW){
+    digitalWrite(3, LOW);
+  } else {
+    digitalWrite(3, HIGH);
+  }
+}
